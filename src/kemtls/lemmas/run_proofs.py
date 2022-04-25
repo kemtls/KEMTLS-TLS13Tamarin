2de89b63cd14ad54eb4d8680fcfbfacdad302941
Thom Wiggers <thom@thomwiggers.nl>
2022-04-25 18:44:15 +0200

    Record more Tamarin output and memory usage

--- src/kemtls/lemmas/run_proofs.py
+++ src/kemtls/lemmas/run_proofs.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 from math import log10
-from ssl import CERT_REQUIRED
 import subprocess
 import re
-from typing import Iterable, List, Optional, Tuple
+import os
+import platform
+import shlex
+from typing import Iterable, Optional, Tuple
 
 #: Fetch lemmas from incomplete output
 LEMMA_REGEX: re.Pattern = re.compile(r"^\s*(?P<lemma>[a-zA-Z0-9_]+) \((all-traces|exists-trace)\): analysis incomplete \(1 steps\)\s*$")
@@ -11,25 +13,46 @@ LEMMA_REGEX: re.Pattern = re.compile(r"^\s*(?P<lemma>[a-zA-Z0-9_]+) \((all-trace
 #: Parse out steps
 STEPS_REGEX: str = r"^\s*{lemma} \((all-traces|exists-trace)\): verified \((?P<steps>\d+) steps\)\s*$"
 
+if platform.uname().system == "Darwin":
+    unit = "K"
+else:
+    unit = "M"
 
-def run_tamarin(model: str, prove: Optional[str] = None) -> str:
-    cmd = ["tamarin-prover", model]
+TIMEFMT: str = f"""%J
+%U user  %S system  %P cpu  %*E total
+avg shared (code):         %X KB
+avg unshared (data/stack): %D KB
+total (sum):               %K KB
+max memory:                %M {unit}B
+"""
+del unit
+
+
+PAGE_SIZE: str = os.sysconf("SC_PAGE_SIZE")
+
+
+def run_tamarin(model: str, prove: Optional[str] = None) -> Tuple[str, str]:
+    cmd = ["time", "tamarin-prover", model]
     if prove is not None:
         modelname = model.removesuffix(".spthy")
         cmd.append(f"--prove={prove}")
         cmd.append(f"--output=proofs/{modelname}_{prove}.spthy")
+    env = os.environ.copy()
+    env["TIMEFMT"] = TIMEFMT
     result = subprocess.run(
-        cmd,
-        check=True,
+        ["/bin/zsh", "-c", shlex.join(cmd)],
         capture_output=True,
         text=True,
+        env=env,
     )
-    return result.stdout
+    timeoutput = '\n'.join(result.stderr.splitlines()[-(1+len(TIMEFMT.splitlines())):])
+    return result.stdout, timeoutput
 
 
 def get_lemmas(model: str) -> Iterable[str]:
     """Parse out the lemmas from the output of Tamarin"""
-    lines = run_tamarin(model).splitlines()
+    output, _ = run_tamarin(model)
+    lines = output.splitlines()
 
     for line in lines:
         if m := LEMMA_REGEX.match(line):
@@ -42,7 +65,21 @@ def get_steps(tamarin_output: str, lemma) -> Optional[str]:
         return match.group("steps")
 
 
-def prove_lemmas(model, lemmas: Iterable[str]):
+def write_output(model: str, lemma: str, output: str):
+    modelname = model.removesuffix(".spthy")
+    with open(f"proofs/{modelname}_{lemma}.out", "w") as fh:
+        fh.write(output)
+
+
+def write_statistics(model: str, lemma: str, steps: str, runtime: str, timeoutput: str):
+    modelname = model.removesuffix(".spthy")
+    with open(f"proofs/{modelname}_{lemma}.stats", "w") as fh:
+        fh.write(f"Runtime: {runtime}\n")
+        fh.write(f"Steps:   {steps}\n")
+        fh.write(timeoutput)
+
+
+def prove_lemmas(model: str, lemmas: Iterable[str]):
     lemmas = list(lemmas)
     max_lemma_length = max(len(lemma) for lemma in lemmas)
     for idx, lemma in enumerate(lemmas):
@@ -50,14 +87,16 @@ def prove_lemmas(model, lemmas: Iterable[str]):
         stridx = str(idx+1).rjust(len(str(len(lemmas))))
         lemma_name = lemma.ljust(max_lemma_length)
         print(f"[{stridx}/{len(lemmas)}] Proving {lemma_name} at {start_time.time()}...", end="", flush=True)
-        output = run_tamarin(model, lemma)
+        output, timeoutput = run_tamarin(model, lemma)
         duration = datetime.now() - start_time
         steps = get_steps(output, lemma)
+        write_output(model, lemma, output)
+        write_statistics(model, lemma, steps, duration, timeoutput)
         if steps is None:
             print(output)
             print(f"Proving {lemma} failed after {duration}!?")
             break
-        print(f"\tCompleted in {steps: >4} steps in {duration}")
+        print(f"\tCompleted in {steps: >6} steps in {duration}")
 
 
 if __name__ == "__main__":
